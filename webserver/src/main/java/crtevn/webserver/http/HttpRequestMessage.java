0cc672bb90093f05e04bb4f89d4c0d8b3fa565e0
Ju, Seonghwan (Ape) <crtevn@gmail.com>
2024-07-10 01:01:56 +0900

    [webserver] Request Message에서 body 파싱 기능 추가

--- webserver/src/main/java/crtevn/webserver/http/HttpRequestMessage.java
+++ webserver/src/main/java/crtevn/webserver/http/HttpRequestMessage.java
@@ -2,16 +2,21 @@ package crtevn.webserver.http;
 
 import crtevn.webserver.http.components.HeaderFields;
 import crtevn.webserver.http.components.HttpMethod;
+import crtevn.webserver.http.components.RequestBody;
 import crtevn.webserver.http.components.RequestLine;
 
 public class HttpRequestMessage {
 
     private final RequestLine requestLine;
     private final HeaderFields headerFields;
+    private final RequestBody requestBody;
 
-    public HttpRequestMessage(RequestLine requestLine, HeaderFields headerFields) {
+    public HttpRequestMessage(RequestLine requestLine, HeaderFields headerFields,
+        RequestBody requestBody
+    ) {
         this.requestLine = requestLine;
         this.headerFields = headerFields;
+        this.requestBody = requestBody;
     }
 
     public HttpMethod getHttpMethod() {
@@ -30,13 +35,24 @@ public class HttpRequestMessage {
         return requestLine.getQueryParam(name);
     }
 
+    public String[] getBodyParameter(String name) {
+        return requestBody.getBodyParam(name);
+    }
+
     public String getHttpVersion() {
         return requestLine.getHttpVersion();
     }
 
     @Override
     public String toString() {
-        return requestLine.toString()
-            + headerFields.toString();
+        if (requestBody.isEmpty()) {
+            return requestLine.toString()
+                + headerFields.toString();
+        } else {
+            return requestLine.toString()
+                + headerFields.toString()
+                + System.lineSeparator()
+                + requestBody.toString();
+        }
     }
 }
